97046bb2e1de4ef8dfd29ccca0a5502900e5ecca
Divyahub7 <divya14042006@gmail.com>
2026-01-24 21:31:36 +0530

    Building Basic Components - I

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Outlet } from "react-router-dom";
-import { Footer, Header } from "./components";
+import { Footer, Header } from "./components/Input";
 
 function App() {
   const [loading, setLoading] = useState(true);
