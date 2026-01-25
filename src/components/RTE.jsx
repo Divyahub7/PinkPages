396a5ac7fb5acabc28356b5410bc668d5dddf485
Divyahub7 <divya14042006@gmail.com>
2026-01-25 16:33:53 +0530

    Building Components - II

---
+++ src/components/RTE.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+function RTE() {
+  return <div>RTE</div>;
+}
+
+export default RTE;
